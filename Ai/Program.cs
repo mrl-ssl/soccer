2a2626f46d43d3eb7429b3ffff9a7938906da145
alireza <rashvandalireza77@gmail.com>
2021-04-30 19:13:05 +0430

    add obstacles

--- Ai/Program.cs
+++ Ai/Program.cs
@@ -30,7 +30,5 @@ namespace MRL.SSL.Ai
                 }
             }
         }
-
-
     }
 }
--- Common/Obstacle.cs
+++ Common/Obstacle.cs
@@ -1,70 +1,8 @@
+using MRL.SSL.Common.Configuration;
 using MRL.SSL.Common.Math;
 
 namespace MRL.SSL.Common
 {
-    public class Obstacle
-    {
-        ObstacleType type;
-        VectorF2D reagion;
-        SingleObjectState state;
-
-        public ObstacleType Type { get => type; set => type = value; }
-        /// <summary>
-        /// length and width of obstacle region
-        /// for circle obstacles size of this vector used
-        /// </summary>
-        public VectorF2D Reagion { get => reagion; set => reagion = value; }
-        public SingleObjectState State { get => state; set => state = value; }
-
-        public bool Meet(SingleObjectState From, SingleObjectState To, float obstacleRadi)
-        {
-            if (IsCircle())
-                return MeetCircle(From, To, obstacleRadi);
-            else
-                return MeetRectangle(From, To, obstacleRadi);
-        }
-
-        public bool Meet(SingleObjectState S1, float obstacleRadi)
-        {
-            // TODO: must check for rectangle
-            Vector2D<float> v = S1.Location - state.Location;
-            return v.Length() < obstacleRadi + reagion.X;
-        }
-
-        private bool MeetRectangle(SingleObjectState From, SingleObjectState To, float obstacleRadi)
-        {
-            VectorF2D N = From.Location;
-            VectorF2D T = To.Location;
-            VectorF2D[] c = new VectorF2D[4];
-
-            c[0] = new VectorF2D(state.Location.X - reagion.X, state.Location.Y - reagion.Y);
-            c[1] = new VectorF2D(state.Location.X + reagion.X, state.Location.Y - reagion.Y);
-            c[2] = new VectorF2D(state.Location.X + reagion.X, state.Location.Y + reagion.Y);
-            c[3] = new VectorF2D(state.Location.X - reagion.X, state.Location.Y + reagion.Y);
-            float d = 0;
-            // check box against oriented sweep
-            for (int i = 0; i < 4; i++)
-            {
-                d = VectorF2D.DistanceSegToSeg(N, T, c[i], c[(i + 1) % 4]);
-                if (d < obstacleRadi) return (true);
-            }
-            return (Meet(new SingleObjectState(N), obstacleRadi) || Meet(new SingleObjectState(T), obstacleRadi));
-        }
-        private bool MeetCircle(SingleObjectState From, SingleObjectState To, float obstacleRadi)
-        {
-            Vector2D<float> p = VectorF2D.PointOnSegment(From.Location, To.Location, state.Location);
-            float d = p.Distance(state.Location);
-            return d <= reagion.X + obstacleRadi;
-        }
-
-        private bool IsCircle() =>
-            type == ObstacleType.Circle ||
-            type == ObstacleType.OurRobot ||
-            type == ObstacleType.OppRobot ||
-            type == ObstacleType.ZoneCircle ||
-            type == ObstacleType.Ball;
-    }
-
     public enum ObstacleType
     {
         Circle,
@@ -72,7 +10,137 @@ namespace MRL.SSL.Common
         OurRobot,
         OppRobot,
         Ball,
-        ZoneCircle,
-        ZoneRectangle
+        OurZone,
+        OppZone
+    }
+
+    public abstract class ObstacleBase
+    {
+        protected SingleObjectState state;
+
+        public abstract ObstacleType Type { get; }
+        public SingleObjectState State { get => state; set => state = value; }
+        public bool Avoid { get; set; }
+
+        protected ObstacleBase(SingleObjectState state) { this.state = state; Avoid = false; }
+        // protected ObstacleBase() { this.state = new SingleObjectState(); }
+
+        public abstract bool Meet(SingleObjectState From, SingleObjectState To, float obstacleRadi, float margin = 0f);
+        public abstract bool Meet(SingleObjectState S1, float obstacleRadi, float margin = 0f);
+    }
+
+    public class CircleObstacle : ObstacleBase
+    {
+        protected float radius;
+
+        public override ObstacleType Type => ObstacleType.Circle;
+
+        public CircleObstacle(SingleObjectState state, float radius) : base(state) { this.radius = radius; }
+
+        public override bool Meet(SingleObjectState From, SingleObjectState To, float obstacleRadi, float margin = 0f)
+        {
+            Vector2D<float> p = VectorF2D.PointOnSegment(From.Location, To.Location, state.Location);
+            float d = p.Distance(state.Location);
+            return d <= radius + obstacleRadi + margin;
+        }
+
+        public override bool Meet(SingleObjectState S1, float obstacleRadi, float margin = 0f)
+        {
+            Vector2D<float> v = S1.Location - state.Location;
+            return v.SqLength() < (obstacleRadi + radius) * (obstacleRadi + radius) + margin;
+        }
+    }
+
+    public class RectObstacle : ObstacleBase
+    {
+        protected float width, heigth;
+
+        public override ObstacleType Type => ObstacleType.Rectangle;
+
+        public RectObstacle(SingleObjectState state, float width, float heigth) : base(state)
+        {
+            this.width = width;
+            this.heigth = heigth;
+        }
+
+        public override bool Meet(SingleObjectState From, SingleObjectState To, float obstacleRadi, float margin = 0f)
+        {
+            Vector2D<float> N = From.Location;
+            Vector2D<float> T = To.Location;
+            Vector2D<float>[] c = new VectorF2D[4];
+
+            c[0] = new VectorF2D(state.Location.X - width, state.Location.Y - heigth);
+            c[1] = new VectorF2D(state.Location.X + width, state.Location.Y - heigth);
+            c[2] = new VectorF2D(state.Location.X + width, state.Location.Y + heigth);
+            c[3] = new VectorF2D(state.Location.X - width, state.Location.Y + heigth);
+            float d = 0;
+            // check box against oriented sweep
+            for (int i = 0; i < 4; i++)
+            {
+                d = VectorF2D.DistanceSegToSeg(N, T, c[i], c[(i + 1) % 4]);
+                if (d < obstacleRadi + margin) return true;
+            }
+            return Meet(From, obstacleRadi, margin) || Meet(To, obstacleRadi, margin);
+        }
+
+        public override bool Meet(SingleObjectState S1, float obstacleRadi, float margin = 0f)
+        {
+            Vector2D<float> v = S1.Location - state.Location;
+            return System.MathF.Abs(v.X) < width + obstacleRadi + margin && System.MathF.Abs(v.Y) < heigth + obstacleRadi + margin;
+        }
+    }
+
+    public abstract class RobotObstacle : CircleObstacle
+    {
+        public int Id { get; }
+
+        protected RobotObstacle(SingleObjectState state, int id) : base(state, RobotConfig.Default.Radius) { Id = id; }
+    }
+
+    public class OurRobotObstacle : RobotObstacle
+    {
+        public override ObstacleType Type => ObstacleType.OurRobot;
+
+        public OurRobotObstacle(SingleObjectState state, int id) : base(state, id) { }
+    }
+
+    public class OppRobotObstacle : RobotObstacle
+    {
+        public override ObstacleType Type => ObstacleType.OppRobot;
+
+        public OppRobotObstacle(SingleObjectState state, int id) : base(state, id) { }
+    }
+
+    public class OurZoneObstacle : RectObstacle
+    {
+        public override ObstacleType Type => ObstacleType.OurZone;
+
+        public OurZoneObstacle()
+            : base(
+                new SingleObjectState(FieldConfig.Default.OurGoalCenter - new VectorF2D(FieldConfig.Default.DefenceAreaHeight / 2, 0f)),
+                FieldConfig.Default.DefenceAreaWidth,
+                FieldConfig.Default.DefenceAreaHeight)
+        {
+        }
+    }
+
+    public class OppZoneObstacle : RectObstacle
+    {
+        public override ObstacleType Type => ObstacleType.OppZone;
+
+        public OppZoneObstacle()
+            : base(new SingleObjectState(
+                FieldConfig.Default.OppGoalCenter + new VectorF2D(FieldConfig.Default.DefenceAreaHeight / 2, 0f)),
+                FieldConfig.Default.DefenceAreaWidth + RobotConfig.Default.Radius * 2,
+                FieldConfig.Default.DefenceAreaHeight + RobotConfig.Default.Radius * 2)
+        {
+        }
+    }
+
+    public class BallObstacle : CircleObstacle
+    {
+        public override ObstacleType Type => ObstacleType.Ball;
+
+        public BallObstacle(SingleObjectState state) : base(state, MergerTrackerConfig.Default.BallRadi) { }
     }
 }
